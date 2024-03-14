2fdf83bc978fdeee9c8c95a71124ce79459c075e
bsubram3 <159208660+bsubram3@users.noreply.github.com>
2024-03-14 19:23:43 -0400

    Update playstreamlit.py

--- playstreamlit.py
+++ playstreamlit.py
@@ -3,3 +3,9 @@ import pandas as pd
 import numpy as np
 
 st.title('Create  Dynamic Visuals')
+
+uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+  dataframe = pd.read_csv(uploaded_file)
+  st.write(dataframe)
