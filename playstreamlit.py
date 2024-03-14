877734edd093cdd6e3f55bdc6c6603235fca3577
bsubram3 <159208660+bsubram3@users.noreply.github.com>
2024-03-14 19:26:49 -0400

    Update playstreamlit.py

--- playstreamlit.py
+++ playstreamlit.py
@@ -9,3 +9,7 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
   dataframe = pd.read_csv(uploaded_file)
   st.write(dataframe)
+  # iterating the columns
+  st.title('Columns in the Dataset')
+  for col in data.columns:
+    st.write((col)
